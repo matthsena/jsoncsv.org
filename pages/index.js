de1e3591f45713c3bef7ff29d3386d7caf06dc1b
Matheus <matheusalexandre.sena99@gmail.com>
2020-06-06 21:38:20 -0300

    Refactore with extending React.Component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import * as  jsonexport from 'jsonexport';
+import * as  jsonexport from 'jsonexport'
 
 let selectedFile = null
 let jsonContent = null
@@ -8,56 +8,6 @@ let textAreaValue = null
 const handleChange = event => {
   textAreaValue = event.target.value
 }
-
-const onFileChange = event => { 
-  selectedFile = event.target.files[0];
-  
-  if (selectedFile) {
-    const reader = new FileReader()
-    reader.readAsText(selectedFile, 'UTF-8')
-    
-    reader.onload = (data) => {
-      jsonContent = JSON.parse(data.target.result)
-    }
-
-
-    reader.onerror = (error) => {
-      console.log(error)
-    }
-  }
-}; 
-
-const onFileUpload = () => { 
-
-  if (textAreaValue) {
-    try {
-
-      textAreaValue = JSON.parse(textAreaValue)
-      jsonexport(textAreaValue,function(err, csv){
-        if(err) return console.log(err);
-        console.log(csv);
-        writeAndDownload(csv, 'converted', 'text/csv')
-      });
-    } catch (error) {
-      console.error(error)
-    }
-
-  } else if (jsonContent) {
-
-    const title = String(selectedFile.name).replace('.json', '')
-
-    jsonexport(jsonContent,function(err, csv){
-      if(err) return console.log(err);
-      console.log(csv);
-      writeAndDownload(csv, title, 'text/csv')
-    });
-
-  } else {
-
-    console.warn('No value')
-  }
-
-}; 
 
 const writeAndDownload = (data, fileName, fileType) => {
 
@@ -78,306 +28,363 @@ const writeAndDownload = (data, fileName, fileType) => {
   }, 0)
 }
 
-const forcedUpload = () => {
-  const e = document.getElementById('uploadInput')
-  e.click();
-}
+class Home extends React.Component {
 
-export default function Home() {
+  constructor(props) {
+    super(props)
+    this.state = {fileName: null}
+  }
+
+  forcedUpload = () => {
+    const e = document.getElementById('uploadInput')
+    e.click();
+  }
+
+  onFileUpload = () => { 
+    if (textAreaValue) {
+      try {
+        
+        textAreaValue = JSON.parse(textAreaValue)
+        
+        jsonexport(textAreaValue,function(err, csv){
+          if(err) return console.log(err);
+          console.log(csv);
+          writeAndDownload(csv, 'converted', 'text/csv')
+        });
+      } catch (error) {
+        console.error(error)
+      }
+    } else if (jsonContent) {
+
+      const title = String(selectedFile.name).replace('.json', '')
+      
+      jsonexport(jsonContent,function(err, csv){
+        if(err) return console.log(err);
+        console.log(csv);
+        writeAndDownload(csv, title, 'text/csv')
+      });
+    } else {
+      console.warn('No value')
+    }
+  }
+
+  onFileChange = event => { 
+    selectedFile = event.target.files[0];
+    if (selectedFile) {
+
+      this.setState({fileName: selectedFile.name})
   
-  return (
-    <div className="container">
-      <Head>
-        <title>JSON to CSV</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta charset="utf-8" />
-        <meta lang="en" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-      </Head>
+      const reader = new FileReader()
+      reader.readAsText(selectedFile, 'UTF-8')
+      
+      reader.onload = (data) => {
+        jsonContent = JSON.parse(data.target.result)
+      }
+      reader.onerror = (error) => {
+        console.log(error)
+      }
+    }
+  }
 
-      <main>
-        <h1 className="title">
-          
-          Free  <span>JSON</span> to <span>CSV</span> converter
-        </h1>
+  render() {
+    return (
+      <div className="container">
+        <Head>
+          <title>JSON to CSV</title>
+          <link rel="icon" href="/favicon.ico" />
+          <meta charset="utf-8" />
+          <meta lang="en" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width,initial-scale=1" />
+        </Head>
 
-        <h2 className="description">
-          Convert unlimited <span>JSON</span> files<br/><br/>
-          <code>// MAXIMUM FILE SIZE: <span>UNLIMITED</span></code>
-        </h2>
-
-        <div className="box">
-            <h3><span>Upload</span> or <span>past</span> your JSON!</h3>
-            <p>Click the button below to upload the file, <label htmlFor="txtJSON"> or paste the text in the input.</label></p>
+        <main>
+          <h1 className="title">
             
-            <div className="actions">
-                <div className="half-relative">
-                <button onClick={ forcedUpload }><img src="/cloud.svg" alt="Vercel Logo" className="btn-img" /> <br></br>Upload JSON file</button>
-                    <input id="uploadInput" type="file"  accept="application/JSON" onChange={onFileChange} /> 
-                </div>
-                <div className="half-relative">
-                  <textarea id="txtJSON" cols="30" rows="5" onChange={handleChange} placeholder="Write or Paste your JSON data"></textarea>
-                </div>
-            </div>
-            <div className="actions">
-                <div className="full-relative">
-                  <button className="convert-btn" onClick={ onFileUpload }>Convert to CSV</button>
-                </div>
-            </div>
-        </div>
+            Free  <span>JSON</span> to <span>CSV</span> converter
+          </h1>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" target="_blank"
-          rel="noopener noreferrer" className="card">
-            <h3>Buy me a coffee &rarr;</h3>
-            <p>Donating any amount you help to keep the site online <span>without ads.</span></p>
+          <h2 className="description">
+            Convert unlimited <span>JSON</span> files<br/><br/>
+            <code>// MAXIMUM FILE SIZE: <span>UNLIMITED</span></code>
+          </h2>
+
+          <div className="box">
+              <h3><span>Upload</span> or <span>past</span> your JSON!</h3>
+              <p>Click the button below to upload the file, <label htmlFor="txtJSON"> or paste the text in the input.</label></p>
+              
+              <div className="actions">
+                  <div className="half-relative">
+                    <button onClick={ this.forcedUpload }><img src="/cloud.svg" alt="Vercel Logo" className="btn-img" /> <br></br>Upload JSON file</button>
+                    <p>{this.state.fileName}</p>
+                    <input id="uploadInput" type="file"  accept="application/JSON" onChange={this.onFileChange} /> 
+                  </div>
+                  <div className="half-relative">
+                    <textarea id="txtJSON" cols="30" rows="5" onChange={handleChange} placeholder="Write or Paste your JSON data"></textarea>
+                  </div>
+              </div>
+              <div className="actions">
+                  <div className="full-relative">
+                    <button className="convert-btn" onClick={ this.onFileUpload }>Convert to CSV</button>
+                  </div>
+              </div>
+          </div>
+
+          <div className="grid">
+            <a href="https://nextjs.org/docs" target="_blank"
+            rel="noopener noreferrer" className="card">
+              <h3>Buy me a coffee &rarr;</h3>
+              <p>Donating any amount you help to keep the site online <span>without ads.</span></p>
+            </a>
+
+            <a href="https://github.com/matthsena/jsoncsv.org" target="_blank"
+            rel="noopener noreferrer" className="card">
+              <h3>See on GitHub &rarr;</h3>
+              <p>Collaborate with the project or just take a look!</p>
+            </a>
+          </div>
+        </main>
+
+        <footer>
+          <a
+            href="https://github.com/matthsena/jsoncsv.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Developed by &nbsp;<span>Matheus Alexandre de Sena</span>{' '}
+            <img src="/github.svg" alt="Vercel Logo" className="logo" />
           </a>
+        </footer>
 
-          <a href="https://github.com/matthsena/jsoncsv.org" target="_blank"
-          rel="noopener noreferrer" className="card">
-            <h3>See on GitHub &rarr;</h3>
-            <p>Collaborate with the project or just take a look!</p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://github.com/matthsena/jsoncsv.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Developed by &nbsp;<span>Matheus Alexandre de Sena</span>{' '}
-          <img src="/github.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-       span {
-          color: #0070f3;
-          font-weight: bold;
-        }
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #eaeaea;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-        code span {
-          color: #000;
-          text-decoration: underline
-        }
-
-        .box {
-          diplay: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-          width: 800px;
-          padding: 1.5rem;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          // background:  #f2f2f2;
-        }
-
-        .actions {
-          margin: 1rem 0 0;
-          display: flex;
-          flex-wrap: wrap;
-        }
-        .full-relative {
-          flex: 0 0 100%;
-          max-width: 100%;
-        }
-        .half-relative {
-          flex: 0 0 50%;
-          max-width: 50%
-        }
-        .half-relative *, .full-relative * {
-          position: relative;
-          transform: translateX(-50%);
-          left: 50%;
-        }
-        input {
-          display: none;
-        }
-        textarea, button {
-          padding: 1rem;
-          font-size: 1rem;
-        }
-
-        .convert-btn {
-          padding: 0.5rem 1rem;
-          background: #ffab00;
-          color: #000
-        }
-        button .btn-img {
-          max-height: 3rem;
-          position: absolute;
-          top: 0.5rem;
-          transform: translateX(-50%);
-          left: 50%;
-        }
-        button {
-          background: #0070f3;
-          font-weight: bold;
-          color: white;
-          border-radius: 10px;
-          border: none;
-          height: 100%;
-          line-height: 2rem;
-        }
-        button:hover {
-          cursor: pointer;
-          opacity: 0.75;
-          transition: opacity .25s linear;
-        }
-        button:active, button:focus, button:target {
-          outline: none;
-        }
-        button:active {
-          background: #000;
-          background-size: 100%;
-          transition: background 0s;
-
-        }
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 2rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3, .box h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p, .box p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 2em;
-        }
-
-        @media (max-width: 600px) {
-          .grid, .box {
-            width: 100%;
+        <style jsx>{`
+          .container {
+            min-height: 100vh;
+            padding: 0 0.5rem;
+            display: flex;
             flex-direction: column;
+            justify-content: center;
+            align-items: center;
           }
-        }
-      `}</style>
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
+          main {
+            padding: 5rem 0;
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
 
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+          footer {
+            width: 100%;
+            height: 100px;
+            border-top: 1px solid #eaeaea;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+
+          footer img {
+            margin-left: 0.5rem;
+          }
+
+          footer a {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+
+          a {
+            color: inherit;
+            text-decoration: none;
+          }
+        span {
+            color: #0070f3;
+            font-weight: bold;
+          }
+          .title a {
+            color: #0070f3;
+            text-decoration: none;
+          }
+
+          .title a:hover,
+          .title a:focus,
+          .title a:active {
+            text-decoration: underline;
+          }
+
+          .title {
+            margin: 0;
+            line-height: 1.15;
+            font-size: 4rem;
+          }
+
+          .title,
+          .description {
+            text-align: center;
+          }
+
+          .description {
+            line-height: 1.5;
+            font-size: 1.5rem;
+          }
+
+          code {
+            background: #eaeaea;
+            border-radius: 5px;
+            padding: 0.75rem;
+            font-size: 1.1rem;
+            font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+              DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+          }
+          code span {
+            color: #000;
+            text-decoration: underline
+          }
+
+          .box {
+            diplay: flex;
+            align-items: center;
+            justify-content: center;
+            flex-wrap: wrap;
+            width: 800px;
+            padding: 1.5rem;
+            border: 1px solid #eaeaea;
+            border-radius: 10px;
+            // background:  #f2f2f2;
+          }
+
+          .actions {
+            margin: 1rem 0 0;
+            display: flex;
+            flex-wrap: wrap;
+          }
+          .full-relative {
+            flex: 0 0 100%;
+            max-width: 100%;
+          }
+          .half-relative {
+            flex: 0 0 50%;
+            max-width: 50%
+          }
+          .half-relative *, .full-relative * {
+            position: relative;
+            transform: translateX(-50%);
+            left: 50%;
+          }
+          input {
+            display: none;
+          }
+          textarea, button {
+            padding: 1rem;
+            font-size: 1rem;
+          }
+
+          .convert-btn {
+            padding: 0.5rem 1rem;
+            background: #ffab00;
+            color: #000
+          }
+          button .btn-img {
+            max-height: 3rem;
+            position: absolute;
+            top: 0.5rem;
+            transform: translateX(-50%);
+            left: 50%;
+          }
+          button {
+            background: #0070f3;
+            font-weight: bold;
+            color: white;
+            border-radius: 10px;
+            border: none;
+            height: 100%;
+            line-height: 2rem;
+          }
+          button:hover {
+            cursor: pointer;
+            opacity: 0.75;
+            transition: opacity .25s linear;
+          }
+          button:active, button:focus, button:target {
+            outline: none;
+          }
+          button:active {
+            background: #000;
+            background-size: 100%;
+            transition: background 0s;
+
+          }
+          .grid {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-wrap: wrap;
+
+            max-width: 800px;
+            margin-top: 2rem;
+          }
+
+          .card {
+            margin: 1rem;
+            flex-basis: 45%;
+            padding: 1.5rem;
+            text-align: left;
+            color: inherit;
+            text-decoration: none;
+            border: 1px solid #eaeaea;
+            border-radius: 10px;
+            transition: color 0.15s ease, border-color 0.15s ease;
+          }
+
+          .card:hover,
+          .card:focus,
+          .card:active {
+            color: #0070f3;
+            border-color: #0070f3;
+          }
+
+          .card h3, .box h3 {
+            margin: 0 0 1rem 0;
+            font-size: 1.5rem;
+          }
+
+          .card p, .box p {
+            margin: 0;
+            font-size: 1.25rem;
+            line-height: 1.5;
+          }
+
+          .logo {
+            height: 2em;
+          }
+
+          @media (max-width: 600px) {
+            .grid, .box {
+              width: 100%;
+              flex-direction: column;
+            }
+          }
+        `}</style>
+
+        <style jsx global>{`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+              sans-serif;
+          }
+
+          * {
+            box-sizing: border-box;
+          }
+        `}</style>
+      </div>
+    )
+  }
 }
+
+
+export default Home
